34606a55fb2250ae63325f3c40beedd85f7316ed
Rocky Luvis <rockyinott24@yahoo.com>
2024-07-31 12:45:34 -0500

    testing

--- template.py
+++ template.py
@@ -30,6 +30,7 @@ for filepath in list_of_files:
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory: {filedir}  filename: {filename}")
+        #Test line
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
